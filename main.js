713e3a0c1b562d1a5766a72b166c91e318392304
HansenWinowatan <138988518+HansenWinowatan@users.noreply.github.com>
2025-05-01 19:11:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-onload = () =>{
-        document.body.classList.remove("container");
+window.onload = () => {
+    document.body.classList.remove("container");
+
+    const music = document.getElementById("bg-music");
+    const button = document.getElementById("playMusic");
+
+    if (button && music) {
+        button.addEventListener("click", () => {
+            music.play();
+        });
+    }
 };
